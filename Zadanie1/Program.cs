3cea779470b8d8b2af79af71ce0cc0ab9d1b92c6
s27655 <s27655@pjwstk.edu.pl>
2026-03-11 19:49:05 +0100

    Question about age added

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Witaj w aplikacji!");
-Console.WriteLine("-------------------");
+﻿Console.WriteLine("Witaj w prostej aplikacji .NET!");
+Console.WriteLine("--------------------------------");
 
 Console.Write("Podaj swoje imię: ");
 string? imie = Console.ReadLine();
@@ -10,10 +10,43 @@ while (string.IsNullOrWhiteSpace(imie))
     imie = Console.ReadLine();
 }
 
+Console.Write("Podaj swój wiek: ");
+string? inputWiek = Console.ReadLine();
+
+int wiek;
+while (!int.TryParse(inputWiek, out wiek) || wiek < 0)
+{
+    Console.Write("Podaj poprawny wiek: ");
+    inputWiek = Console.ReadLine();
+}
+
 Console.WriteLine();
 Console.WriteLine($"Cześć, {imie}!");
-Console.WriteLine($"Twoje imię zapisane wielkimi literami: {imie.ToUpper()}");
-Console.WriteLine("Dziękuję za skorzystanie z aplikacji!");
+Console.WriteLine($"Masz {wiek} lat.");
+
+if (wiek < 18)
+{
+    Console.WriteLine("Jesteś niepełnoletni.");
+}
+else
+{
+    Console.WriteLine("Jesteś pełnoletni.");
+}
+
+int lataDo100 = 100 - wiek;
+
+if (lataDo100 > 0)
+{
+    Console.WriteLine($"Do 100 lat brakuje Ci jeszcze {lataDo100} lat.");
+}
+else if (lataDo100 == 0)
+{
+    Console.WriteLine("Masz dokładnie 100 lat!");
+}
+else
+{
+    Console.WriteLine("Masz ponad 100 lat!");
+}
 
 Console.WriteLine();
 Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
