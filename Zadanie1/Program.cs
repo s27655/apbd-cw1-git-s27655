48c087ca6a2330af1abfbcb1c3a316d91004ce83
s27655 <s27655@pjwstk.edu.pl>
2026-03-11 19:41:53 +0100

    Greeting added

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Witaj w prostej aplikacji .NET!");
+Console.WriteLine("--------------------------------");
+
+Console.Write("Podaj swoje imię: ");
+string? imie = Console.ReadLine();
+
+while (string.IsNullOrWhiteSpace(imie))
+{
+    Console.Write("Imię nie może być puste. Podaj swoje imię: ");
+    imie = Console.ReadLine();
+}
+
+Console.WriteLine();
+Console.WriteLine($"Cześć, {imie}!");
+Console.WriteLine("Miło Cię poznać.");
+Console.WriteLine("Dziękuję za skorzystanie z aplikacji!");
+
+Console.WriteLine();
+Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+Console.ReadKey();
